b83959b1d4a4dc592a1528c897e2a34b16dcb6d7
Igor Toigildin <toygildinigor@gmail.com>
2025-01-09 14:30:32 +0300

    add new cobra commands

--- internal/client/grpc/app/app.go
+++ internal/client/grpc/app/app.go
@@ -38,7 +38,7 @@ var (
 	}
 )
 
-// registration
+// user registration
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "New user registration",
@@ -68,7 +68,7 @@ var createUserCmd = &cobra.Command{
 	},
 }
 
-// login
+// user login
 var loginCmd = &cobra.Command{
 	Use:   "login",
 	Short: "User authentication",
@@ -94,6 +94,105 @@ var loginUserCmd = &cobra.Command{
 		}
 
 		log.Printf("user with %s email logged in successfully\n", emailStr)
+	},
+}
+
+// save command
+var saveCmd = &cobra.Command{
+	Use:   "save",
+	Short: "Save data in storage",
+}
+
+// save password subcommand
+var savePasswordCmd = &cobra.Command{
+	Use:   "password",
+	Short: "Save login && password in storage",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		loginStr, err := cmd.Flags().GetString("login")
+		if err != nil {
+			log.Fatalf("failed to get login: %s\n", err.Error())
+		}
+
+		passStr, err := cmd.Flags().GetString("password")
+		if err != nil {
+			log.Fatalf("failed to get password: %s\n", err.Error())
+		}
+
+		// TODO: save login && pass in minio
+
+		log.Printf("login %s && password %s saved successfully\n", loginStr, passStr)
+	},
+}
+
+// save text subcommand
+var saveTextCmd = &cobra.Command{
+	Use:   "text",
+	Short: "Save arbitrary text data in storage",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		_, err := cmd.Flags().GetString("file_name")
+		if err != nil {
+			log.Fatalf("failed to get file_name: %s\n", err.Error())
+		}
+
+		_, err = cmd.Flags().GetString("text")
+		if err != nil {
+			log.Fatalf("failed to get text: %s\n", err.Error())
+		}
+
+		// TODO: save text in minio
+
+		log.Println("text saved successfully\n")
+	},
+}
+
+// save bin data subcommand
+var saveBinCmd = &cobra.Command{
+	Use:   "bin",
+	Short: "Save binary data in storage",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		_, err := cmd.Flags().GetString("file_name")
+		if err != nil {
+			log.Fatalf("failed to get file_name: %s\n", err.Error())
+		}
+
+		pathStr, err := cmd.Flags().GetString("path")
+		if err != nil {
+			log.Fatalf("failed to get path: %s\n", err.Error())
+		}
+
+		// TODO: save binary data in minio
+
+		log.Printf("biniry data %s saved successfully\n", pathStr)
+	},
+}
+
+// save card bank details subcommand
+var saveCardInfoCmd = &cobra.Command{
+	Use:   "card",
+	Short: "Save bank card details in storage",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		_, err := cmd.Flags().GetString("card_number")
+		if err != nil {
+			log.Fatalf("failed to get card_number: %s\n", err.Error())
+		}
+
+		_, err = cmd.Flags().GetString("CVC")
+		if err != nil {
+			log.Fatalf("failed to get path: %s\n", err.Error())
+		}
+
+		_, err = cmd.Flags().GetString("Expiration date")
+		if err != nil {
+			log.Fatalf("failed to get path: %s\n", err.Error())
+		}
+
+		// TODO: save bank card details in minio
+
+		log.Println("bank card details saved successfully\n")
 	},
 }
 
